dae972c8b120f2fa3417fd0cecd9315828aaa784
Petar Milic <petar020201@hotmail.com>
2025-03-28 10:58:41 +0100

    added mkdir to bash script

--- project/build.sh
+++ project/build.sh
@@ -1,3 +1,4 @@
+mkdir -p bin
 go build -o main/elev_sys elevator-system/main.go
 wget -O bin/hall_request_assigner https://github.com/TTK4145/Project-resources/releases/download/v1.1.3/hall_request_assigner
 chmod +x bin/hall_request_assigner
